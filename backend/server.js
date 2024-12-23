aa67f9ff783247057c98b06b34c6240c6462a90e
Deep Baishya <deepbaishya4385@gmail.com>
2024-12-23 18:55:14 +0530

    env

--- backend/server.js
+++ backend/server.js
@@ -16,11 +16,11 @@ app.use(cors());
 
 // MySQL Database connection
 const dbConfig = {
-  host:'mysql.railway.internal', // Default value in case env variable is not set
-  user: 'root',
-  password:  'ZXXpbahTXoxLeVYxeGIMpdjdruSZqRqv',
-  database:'railway',
-  port: '3306'
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || '',
+  database: process.env.DB_NAME || 'devices_db',
+  port: process.env.DB_PORT || 3306,
 };
 
 
