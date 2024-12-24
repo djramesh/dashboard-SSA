8b5da2df7727c20ec27ceedf3d1382094c8e9698
Deep Baishya <deepbaishya4385@gmail.com>
2024-12-24 11:32:08 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -20,18 +20,22 @@ app.use(cors(corsOptions));
 
 
 // MySQL Database connection
-const dbConfig = {
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
-};
+// const dbConfig = {
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+//   port: process.env.DB_PORT,
+//   connectTimeout: 10000,
+// };
+
+const urlDB = `mysql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.
+DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
 
 //changes here
 // Initialize database and create table if not exists
 const initializeDatabase = async () => {
-  const connection = await mysql.createConnection(dbConfig);
+  const connection = await mysql.createConnection(urlDB);
   await connection.execute(`
     CREATE TABLE IF NOT EXISTS devices (
       id INT PRIMARY KEY,
